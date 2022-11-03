0d235b5d8f8e7e7f5efcd06ad67cf434cffdc086
SARB <mrsinghbusiness05@gmail.com>
2022-11-03 11:08:31 +0530

    Cart Done

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Cart.css';
+import Footer from '../Components/Footer';
+
 
 import { useSelector , useDispatch} from 'react-redux';    // useselector is imported 
 import { remove } from '../Slice/CartSlice';
@@ -15,24 +17,35 @@ import { remove } from '../Slice/CartSlice';
   }
   return (
     <div> 
-         {
+
+         
+         {  products?.length == '' ?  
+         <div style  = {{fontSize:'30px',fontWeight:'500',padding:'5% 5%'}}>  Cart is Empty .....
+          </div> : (
+
            products.map(one => (
-              <div className = "card-products"   key = {one.id}>
-                <div className  = "card-image" style = {{paddingTop:'9%'}}>
-                    <span> <img src = {one.image} style = {{width:'10%'}} /> </span>
+              <div className = "cart-products"   key = {one.id}>
+
+                <div className="card-first">
+                    <div className  = "card-image" style = {{paddingTop:'9%'}}>
+                          <span> <img src = {one.image} style = {{width:'20%'}} /> </span>
+                      </div>
+                      <div className = "card-content">
+                          <div className = "card-price">
+                              <span style = {{fontWeight:'700'}}> Rs.-  {one.price}  </span>
+                          </div>
+                          <div className="card-title">
+                            <span style = {{fontSize:'22px',fontWeight:'400'}}> {one.title} </span>
+                          </div>
+                      </div>
                 </div>
-                <div className = "card-content">
-                    <div className = "card-price">
-                        <span style = {{fontWeight:'700'}}> Rs.-  {one.price}  </span>
-                    </div>
-                    <div className="card-title">
-                      <span style = {{fontSize:'18px',fontWeight:'400'}}> {one.title} </span>
-                    </div>
+                <div className="card-second">
+                   <button id = "cart-remove-btn" onClick={() =>handleremove(one.id)}> Remove </button>
                 </div>
-                <button onClick={() =>handleremove(one.id)}> Remove </button>
-              </div>
+              </div>)
            ))
          }
+         {/* <Footer /> */}
     </div>
   )
 }
